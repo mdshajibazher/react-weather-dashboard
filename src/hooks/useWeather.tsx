13ebe4d4f8dbaf2a854fa3298620235ad05341c4
mdshajibazher <mdshajibazher@gmail.com>
2024-04-27 15:21:31 +0600

    save

--- src/hooks/useWeather.tsx
+++ src/hooks/useWeather.tsx
@@ -32,11 +32,28 @@ const useWeather = () => {
 
             const response = await  fetch(``)
             if(!response.ok){
+                const errorMessage = `Fetching weather data failed: ${response.status}`;
+                throw new Error(errorMessage);
+            }
 
+            const data = await response.json();
+            const updatedWeatherData = {
+                ...weatherData,
+                name: data?.name,
+                climate: '',
+                temperature: data?.main?.temp,
+                maxTemperature: data?.main?.temp_max,
+                minTemperature: data?.main?.temp_min,
+                humidity: data?.main?.humidity,
+                cloudPercentage: data?.clouds?.all,
+                wind: data?.wind?.speed,
+                time: data?.dt,
+                longitude: lng,
+                latitude: lat,
             }
         }catch (e)  {
-
-        } finally () {
+            console.log(e)
+        } finally {
             setLoading({
                 ...loading,
                 state: false,
